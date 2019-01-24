c5aa8cdfddd712e1e80fec462a329837ef8fd55e
nico91470 <nicolas.stophe@live.com>
2019-01-24 22:29:50 +0100

    Updates

---
+++ precache-manifest.61c3b80bc1a83961791a264526f8022e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eba08660649e959d48ed",
+    "url": "/faceregognitionbrain/static/js/runtime~main.eba08660.js"
+  },
+  {
+    "revision": "22352ce146c7ed8ae807",
+    "url": "/faceregognitionbrain/static/js/main.22352ce1.chunk.js"
+  },
+  {
+    "revision": "2c1cf6721ee6e5af77fc",
+    "url": "/faceregognitionbrain/static/js/1.2c1cf672.chunk.js"
+  },
+  {
+    "revision": "22352ce146c7ed8ae807",
+    "url": "/faceregognitionbrain/static/css/main.2fc3a659.chunk.css"
+  },
+  {
+    "revision": "2c1cf6721ee6e5af77fc",
+    "url": "/faceregognitionbrain/static/css/1.0fbd0d7f.chunk.css"
+  },
+  {
+    "revision": "b6c4d9bebf4b9fd3535f5d0c6ec55086",
+    "url": "/faceregognitionbrain/index.html"
+  }
+];
